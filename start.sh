3c3f0b8a85d4f3061e7971439a063cba899bb82c
gaoweihua <811055695@qq.com>
2021-05-28 15:59:09 +0800

    mongo指定配置文件

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod a+x /home/www/limspro/onlyoffice/exec && chmod a+x /home/www/limspro_km/onlyoffice/exec
 service apache2 restart
-mongod --dbpath=/root/mongo_data  --fork --logpath=/var/log/mongodb/mongodb.log
+mongod --dbpath=/root/mongo_data --fork --logpath=/var/log/mongodb/mongodb.log --config=/etc/mongod.conf
 /home/www/limspro/webpage/shell/autoExecution.sh
