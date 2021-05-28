286f7accbc3576c2bbe9613f42ed931da93bfc5d
gaoweihua <811055695@qq.com>
2021-05-28 11:56:16 +0800

    exec 文件添加可执行权限

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 service apache2 restart
 #tail -f /dev/null
 /home/www/limspro/webpage/shell/autoExecution.sh
+chmod a+x /home/www/limspro/onlyoffice/exec && chmod a+x /home/www/limspro_km/onlyoffice/exec
